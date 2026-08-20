a7b6abf9ee7b5cd16717b6579efb2441bfdcca1d
Victor Cardoso Cunha <105755943+VictorCardosoOl@users.noreply.github.com>
2026-08-20 18:07:23 -0300

    Atualização estrutural
    Foi implementado o plugin do vite, bem como houve a remodelagem da estrutura dos arquivos bem como alteração de algumas bibliotecas

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './src/data/config.json'), 'utf-8'));
 
 export default defineConfig({
   base: './',
+  plugins: [
+    handlebars({
+      context: configData,
+    }),
+  ],
   build: {
     target: 'es2022',
     assetsInlineLimit: 4096,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        gallery: path.resolve(__dirname, 'gallery.html'),
+      },
       output: {
         manualChunks: {
           vendor: ['gsap', 'lenis'],
